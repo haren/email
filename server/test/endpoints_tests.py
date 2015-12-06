5a33c4b52e3c13ed14f0ba9238ecfb3eefca739f
haren <lukasz.harezlak@gmail.com>
2015-12-06 13:12:15 +0100

    Tests update - sending json now as well.

--- server/test/endpoints_tests.py
+++ server/test/endpoints_tests.py
@@ -10,7 +10,7 @@ class UrlTestCase(unittest.TestCase):
     def test_incorrect_get(self):
         response = requests.get('http://%s:%s/WRONG_URL' % (config.HOST, config.PORT))
 
-        self.assertEqual({"status": 404, "msg": "Incorrect request url."}, response.json())
+        self.assertEqual({"status": 404, "msg": "Incorrect request url /WRONG_URL."}, response.json())
         self.assertEqual(200, response.status_code)
 
     def test_correct_main_page_get(self):
@@ -30,7 +30,7 @@ class UrlTestCase(unittest.TestCase):
     def test_send_correct_email_post(self):
         email_data = {'to': 'lukasz.harezlak@gmail.com', 'subject': 'test', 'text': 'test'}
         response = requests.post(
-            'http://%s:%s/emails' % (config.HOST, config.PORT), data=email_data)
+            'http://%s:%s/emails' % (config.HOST, config.PORT), json=email_data)
 
         self.assertIn("send_status", response.text)
         self.assertEqual(200, response.status_code)
@@ -40,7 +40,7 @@ class UrlTestCase(unittest.TestCase):
     def test_incorrect_to_address_email_post(self):
         email_data = {'to': 'xxxx@@@@not', 'subject': 'test', 'text': 'test'}
         response = requests.post(
-            'http://%s:%s/emails' % (config.HOST, config.PORT), data=email_data)
+            'http://%s:%s/emails' % (config.HOST, config.PORT), json=email_data)
 
         self.assertEqual(200, response.status_code)
         self.assertEqual(500, response.json()['status'])
@@ -50,7 +50,7 @@ class UrlTestCase(unittest.TestCase):
     def test_empty_subject_email_post(self):
         email_data = {'to': config.FROM_ADDRESS, 'subject': '', 'text': 'test'}
         response = requests.post(
-            'http://%s:%s/emails' % (config.HOST, config.PORT), data=email_data)
+            'http://%s:%s/emails' % (config.HOST, config.PORT), json=email_data)
 
         self.assertEqual(200, response.status_code)
         self.assertEqual(500, response.json()['status'])
@@ -60,7 +60,7 @@ class UrlTestCase(unittest.TestCase):
     def test_empty_body_email_post(self):
         email_data = {'to': config.FROM_ADDRESS, 'subject': 'subject', 'text': ''}
         response = requests.post(
-            'http://%s:%s/emails' % (config.HOST, config.PORT), data=email_data)
+            'http://%s:%s/emails' % (config.HOST, config.PORT), json=email_data)
 
         self.assertEqual(200, response.status_code)
         self.assertEqual(500, response.json()['status'])
