08fa8cbaaea1f84484df02d991118eeb8f64de81
haren <lukasz.harezlak@gmail.com>
2015-12-07 13:51:08 +0100

    minified resources included and removed console logs

--- www/js/app/bootstrap.js
+++ www/js/app/bootstrap.js
@@ -1,8 +1,8 @@
 require.config({
 	paths: {
-		jquery: '../libs/jquery/jquery', // 2.1.4
-		underscore: '../libs/underscore/underscore', // 1.8.3
-		backbone: '../libs/backbone/backbone', // 1.2.3,
+		jquery: '../libs/jquery/jquery.min', // 2.1.4
+		underscore: '../libs/underscore/underscore.min', // 1.8.3
+		backbone: '../libs/backbone/backbone.min', // 1.2.3,
 		toastr: '../libs/toastr/toastr.min'
 	},
 	shim: {
